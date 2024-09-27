42ab81158bf3ee7044f6db5b5f559da3f253daa4
sergakad <kadsereg@gmail.com>
2024-09-27 18:29:46 +0300

    комментарий alt

--- src/component/Item.jsx
+++ src/component/Item.jsx
@@ -4,7 +4,7 @@ function Item (props) {
 
     return <div className="card item">
         <div className="card-image waves-effect waves-block waves-light">
-            <img className="activator" src={props.images} alt={props.name}  />
+            <img className="activator" src={props.images} alt={props.name + '\t' + 'Картинка че-то не подгрузилась'} />
         </div>
         <div className="card-content">
             {/*<p className="card-title activator grey-text text-darken-4">{props.name}</p>*/}
