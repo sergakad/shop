d6ae2f536e44aead7192a82442a7b651307b61cf
sergakad <kadsereg@gmail.com>
2024-09-27 18:17:47 +0300

    modern basket

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,10 +7,7 @@ function Header () {
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo" >React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    {/*<li><a href="!#">Repo</a></li>*/}
-                    <div className='basket-top'>
-                        <i className="small material-icons"><a className='basket-top' href="#!">shopping_cart</a></i>
-                    </div>
+                    <li><a href="https://github.com/sergakad/shop">Repo</a></li>
                 </ul>
             </div>
         </nav>
